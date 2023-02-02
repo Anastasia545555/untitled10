c374ef33a065cae89e419457ff62f397aa653987
Anastasiya <lazycat73332@gmail.com>
2023-02-03 02:22:04 +0300

    Второе изменение (добавлены методы stat(), main())

--- src/SalesManager.java
+++ src/SalesManager.java
@@ -14,4 +14,30 @@ public class SalesManager {
         }
         return max;
     }
+
+    public int stat() {
+        if (sales.length != 0) {
+            int st = 0;
+            int max = 0;
+            int min = 0;
+            for (int sale : sales) {
+                if (sale > max) {
+                    max = sale;
+                }
+                if (sale < min) {
+                    min = sale;
+                }
+                st += sale;
+            }
+            st = (st - max - min) / sales.length;
+            return st;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args){
+        int arr[]={10, 12, 15, 42, 1};
+        SalesManager sm=new SalesManager(arr);
+        System.out.println(sm.stat());
+    }
 }
